cfa03ba688b9a44ef1920e8a37a04eefc8397a8a
Olav Vahtras <vahtras@kth.se>
2015-04-08 09:34:33 +0200

    Cp spnorb_queries from dal

--- spnorb_queries.F90
+++ spnorb_queries.F90
@@ -7,13 +7,15 @@ implicit none
 
 contains
     function is_one_electron(label) result(is_one)
-    use infhso_mod, only: one_electron_spin_orbit_labels, mean_field_spin_orbit_labels
+    use infhso_mod, only: one_electron_spin_orbit_labels, &
+        mean_field_spin_orbit_labels, scaled_spin_orbit_labels
     use list_queries, only: is_in_list
     implicit none
     character*8 label
     logical is_one
     is_one = is_in_list(label, one_electron_spin_orbit_labels).or. &
-        is_in_list(label, mean_field_spin_orbit_labels)
+        is_in_list(label, mean_field_spin_orbit_labels).or. &
+        is_in_list(label, scaled_spin_orbit_labels)
     end function is_one_electron
 
     function is_valid_spin_orbit(label) result(is_valid)
@@ -27,6 +29,14 @@ contains
         is_in_list(label, two_electron_spin_orbit_labels).or. &
         is_in_list(label, full_electron_spin_orbit_labels)
     end function is_valid_spin_orbit
+
+    function is_full_spin_orbit(label) result(is_valid)
+    use infhso_mod, only: full_electron_spin_orbit_labels
+    use list_queries, only: is_in_list
+    character*8 label
+    logical is_valid
+    is_valid = is_in_list(label, full_electron_spin_orbit_labels)
+    end function is_full_spin_orbit
 
 end module spnorb_queries
     
